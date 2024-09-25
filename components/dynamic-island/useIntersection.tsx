d4bfa87c8739843646424b9031684641712a6d75
Cuzeac Florin <cuzeacflo@gmail.com>
2024-09-25 12:13:25 +0200

    correct errors

---
+++ components/dynamic-island/useIntersection.tsx
@@ -0,0 +1,25 @@
+import type React from 'react';
+import { useEffect, useState } from 'react';
+
+export const useIntersection = (
+	element: React.MutableRefObject<HTMLElement | null>,
+	rootMargin = '0px',
+) => {
+	const [isVisible, setState] = useState(false);
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				setState(entry.isIntersecting);
+			},
+			{ rootMargin },
+		);
+		element.current && observer.observe(element.current);
+		const current = element.current;
+		return () => {
+			if (current) {
+				observer.unobserve(current);
+			}
+		};
+	}, [element, rootMargin]);
+	return isVisible;
+};
