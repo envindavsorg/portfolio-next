24ff1f3d60e7dffb919571e8040308a17b0f3c3c
Cuzeac Florin <cuzeacflo@gmail.com>
2024-09-30 16:32:29 +0200

    update content & fix bugs

--- content/ContactMe.tsx
+++ content/ContactMe.tsx
@@ -3,40 +3,53 @@ import {
 	ChatCircle,
 	ChatsCircle,
 	EnvelopeSimple,
+	GithubLogo,
 	LinkedinLogo,
 	Phone,
 } from '@phosphor-icons/react/dist/ssr';
 import type React from 'react';
 
 export interface ContactMe {
-	title: string;
+	name: string;
+	description: string;
 	url: string;
 	icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 export const contactMe: ContactMe[] = [
 	{
-		title: 'Envoyez-moi un message sur LinkedIn',
-		url: `https://www.linkedin.com/in/${env.NEXT_PUBLIC_WEBSITE_PREFIX}`,
-		icon: LinkedinLogo,
+		name: 'contact',
+		description: "N'hésitez pas à m'envoyer un message",
+		url: `/`,
+		icon: ChatsCircle,
 	},
 	{
-		title: "N'hésitez pas à m'appeler",
-		url: `tel:${env.NEXT_PUBLIC_PHONE}`,
-		icon: Phone,
-	},
-	{
-		title: "N'hésitez pas à m'envoyer un SMS",
-		url: `sms:${env.NEXT_PUBLIC_PHONE}`,
-		icon: ChatCircle,
-	},
-	{
-		title: "N'hésitez pas à m'envoyer un email",
+		name: 'e-mail',
+		description: "N'hésitez pas à m'envoyer un email",
 		url: `mailto:${env.NEXT_PUBLIC_EMAIL}`,
 		icon: EnvelopeSimple,
 	},
 	{
-		title: "N'hésitez pas à m'envoyer un message",
-		url: `/`,
-		icon: ChatsCircle,
+		name: 'linkedin',
+		description: 'Envoyez-moi un message sur LinkedIn',
+		url: `https://www.linkedin.com/in/${env.NEXT_PUBLIC_WEBSITE_PREFIX}`,
+		icon: LinkedinLogo,
+	},
+	{
+		name: 'github',
+		description: 'Envoyez-moi un message sur LinkedIn',
+		url: `https://github.com/${env.NEXT_PUBLIC_GITHUB_USERNAME}`,
+		icon: GithubLogo,
+	},
+	{
+		name: 'téléphone',
+		description: "N'hésitez pas à m'appeler",
+		url: `tel:${env.NEXT_PUBLIC_PHONE}`,
+		icon: Phone,
+	},
+	{
+		name: 'SMS',
+		description: "N'hésitez pas à m'envoyer un SMS",
+		url: `sms:${env.NEXT_PUBLIC_PHONE}`,
+		icon: ChatCircle,
 	},
 ];
