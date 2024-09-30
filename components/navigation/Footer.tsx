5dd51dc3c218a6a5ed06d2c37913dc62eb30e468
Cuzeac Florin <cuzeacflo@gmail.com>
2024-09-30 16:32:32 +0200

    update content & fix bugs

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,54 +1,70 @@
+import { websiteStatus } from '@/actions/status/status.action';
 import { Separator } from '@/components/ui/Separator';
+import { type ContactMe, contactMe } from '@/content/ContactMe';
 import { env } from '@/env/client';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 import type React from 'react';
 
-interface Links {
-	name: string;
-	url: string;
-}
-
-export const Footer = (): React.JSX.Element => {
-	const links: Links[] = [
-		{
-			name: 'contact',
-			url: '/contact',
-		},
-		{
-			name: 'e-mail',
-			url: `mailto:${env.NEXT_PUBLIC_EMAIL}`,
-		},
-		{
-			name: 'linkedin',
-			url: `https://www.linkedin.com/in/${env.NEXT_PUBLIC_WEBSITE_PREFIX}`,
-		},
-		{
-			name: 'github',
-			url: `https://github.com/${env.NEXT_PUBLIC_GITHUB_USERNAME}`,
-		},
-	];
+export const Footer = async (): Promise<React.JSX.Element> => {
+	const { label, color, status } = await websiteStatus(
+		env.NEXT_PUBLIC_WEBSITE_PREFIX,
+	);
 
 	return (
 		<footer className="mt-12 text-center">
-			<div className="flex justify-center space-x-4 tracking-tight">
-				{links.map((link) => (
-					<a
-						key={link.name}
-						href={link.url}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="text-foreground transition-colors duration-200 hover:text-theme"
-					>
-						{link.name}
-					</a>
-				))}
+			<div className="flex justify-center space-x-4 tracking-tight sm:space-x-6">
+				{contactMe
+					.slice(0, 4)
+					.map(({ name, url, description }: ContactMe, idx: number) => (
+						<a
+							key={`${idx}-${name}`}
+							href={url}
+							aria-label={description}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="text-foreground transition-colors duration-200 hover:text-theme"
+						>
+							{name}
+						</a>
+					))}
 			</div>
+
 			<div className="my-3 flex items-center justify-center">
-				<Separator className="w-6" />
+				<Separator className="!bg-foreground w-6" />
 			</div>
+
 			<p className="text-sm">
 				<span className="font-bold">{new Date().getFullYear()}</span> - Cuzeac
 				Florin
 			</p>
+
+			<div className="mt-6 inline-flex max-w-fit items-center gap-x-2 sm:gap-x-3">
+				<Link
+					href="https://cuzeacflorin.openstatus.dev"
+					target="_blank"
+					rel="noreferrer"
+					className="font-semibold text-xs sm:text-sm"
+					passHref
+				>
+					{label}
+				</Link>
+
+				<span className="relative flex size-2">
+					{status === 'operational' ? (
+						<span
+							className={cn(
+								'absolute inline-flex size-full rounded-full',
+								'animate-ping opacity-75 duration-1000',
+								color,
+							)}
+						/>
+					) : null}
+					<span
+						className={cn('relative inline-flex size-2 rounded-full', color)}
+					/>
+				</span>
+			</div>
 		</footer>
 	);
 };
