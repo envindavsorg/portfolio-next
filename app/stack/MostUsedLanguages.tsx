e3cdd847f5ac351ee500d5421a22e20424a6fef6
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-02 09:04:19 +0200

    update github page

--- app/stack/MostUsedLanguages.tsx
+++ app/stack/MostUsedLanguages.tsx
@@ -1,10 +1,11 @@
 import { wakatimeStats } from '@/actions/wakatime/stats.action';
 import { MostUsedLanguagesCharts } from '@/app/stack/MostUsedLanguagesCharts';
+import { env } from '@/env/server';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export const MostUsedLanguages = async () => {
 	noStore();
-	const { data } = await wakatimeStats();
+	const { data } = await wakatimeStats(env.WAKATIME_DATA_URL);
 
 	return <MostUsedLanguagesCharts languages={data.languages} />;
 };
