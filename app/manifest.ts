641400192eedff36408090b3037e8f720d1db53a
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-09 21:15:28 +0200

    add manifest

---
+++ app/manifest.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from 'next';
+
+const manifest = (): MetadataRoute.Manifest => ({
+	name: 'Mon portfolio personnel',
+	short_name: 'Cuzeac Florin',
+	description:
+		"Développeur web, passionné par la création d'applications belles et fonctionnelles, et de nouvelles technologies.",
+	start_url: '/',
+	display: 'standalone',
+	background_color: '#000000',
+	theme_color: '#000000',
+	icons: [
+		{
+			src: '/favicon.ico',
+			sizes: 'any',
+			type: 'image/x-icon',
+		},
+	],
+});
+
+export default manifest;
