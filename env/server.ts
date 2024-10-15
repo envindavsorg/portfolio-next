c494943e721d2af60dadce2da1365086a07cc7db
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-15 17:37:42 +0200

    optimize components

--- env/server.ts
+++ env/server.ts
@@ -19,6 +19,9 @@ export const env = createEnv({
 		TURBO_TEAM: z.string().min(1),
 		UPSTASH_REDIS_REST_URL: z.string().url(),
 		UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
+		AUTH_SECRET: z.string().min(1),
+		AUTH_GITHUB_ID: z.string().min(1),
+		AUTH_GITHUB_SECRET: z.string().min(1),
 	},
 	runtimeEnv: {
 		GITHUB_TOKEN: process.env.GITHUB_TOKEN,
@@ -37,5 +40,8 @@ export const env = createEnv({
 		TURBO_TEAM: process.env.TURBO_TEAM,
 		UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
 		UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+		AUTH_SECRET: process.env.AUTH_SECRET,
+		AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+		AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
 	},
 });
