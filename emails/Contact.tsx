24e7c4f72ee7823d829709ceace2d3666b8cb7a3
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-15 13:57:27 +0200

    update mail

--- emails/Contact.tsx
+++ emails/Contact.tsx
@@ -8,7 +8,9 @@ import {
 	Preview,
 	Text,
 } from '@react-email/components';
+import { Tailwind } from '@react-email/tailwind';
 import type React from 'react';
+import config from '../tailwind.config';
 
 interface ContactEmailTemplateProps {
 	name: string;
@@ -23,108 +25,62 @@ const ContactEmailTemplate = ({
 	mail,
 	content,
 }: Readonly<ContactEmailTemplateProps>): React.JSX.Element => (
-	<Html>
-		<Head />
-		<Preview>
-			{name} {surname} vous a envoyé un nouveau message :
-		</Preview>
-		<Body
-			style={{
-				backgroundColor: '#FFFFFF',
-			}}
-		>
-			<Container
-				style={{
-					paddingLeft: '12px',
-					paddingRight: '12px',
-					margin: '0 auto',
-				}}
-			>
-				<Heading
-					style={{
-						color: '#333333',
-						fontSize: '24px',
-						fontWeight: 'bold',
-						margin: '40px 0',
-						padding: '0',
-					}}
-				>
-					Driiiing, un nouveau message t'attend, depuis ton site ! 📩
-				</Heading>
-				<Text
-					style={{
-						fontSize: '14px',
-						margin: '24px 0',
-						display: 'block',
-						marginBottom: '4px',
-					}}
-				>
-					Florin, tu as reçu un nouveau message, depuis ton site, de la part de{' '}
-					<span
-						style={{
-							fontWeight: 'bold',
-						}}
+	<Tailwind config={config}>
+		<Html>
+			<Head />
+			<Preview>
+				{name} {surname} vous a envoyé un nouveau message :
+			</Preview>
+			<Body className="bg-black">
+				<Container className="mx-auto px-3">
+					<Heading className="font-extrabold text-2xl text-white">
+						<span className="font-bold text-yellow-300">Driiiing</span>, un
+						nouveau message t'attend, depuis ton site !
+					</Heading>
+					<Text className="mt-20 block text-base text-white">
+						<span className="font-bold text-yellow-300">Florin</span>, tu as
+						reçu un nouveau message, depuis ton site, de la part de{' '}
+						<span className="font-bold">
+							{name} {surname}
+						</span>{' '}
+						qui a pour adresse e-mail{' '}
+						<span className="font-extrabold text-yellow-300 underline">
+							{mail}
+						</span>
+						.
+					</Text>
+					<Text className="mt-10 block text-base text-white">
+						Voici le contenu :
+					</Text>
+					<Text
+						className="mt-1 bg-transparent p-4 font-bold text-white text-xl"
+						style={{ border: '1px solid #FFFFFF', borderRadius: '10px' }}
 					>
-						{name} {surname}
-					</span>{' '}
-					qui a pour adresse e-mail{' '}
-					<span
-						style={{
-							fontWeight: 'bold',
-							textDecoration: 'underline',
-						}}
-					>
-						{mail}
-					</span>
-					. Voici le contenu :
-				</Text>
-				<Text
-					style={{
-						display: 'inline-block',
-						padding: '16px 4.5%',
-						width: '90.5%',
-						backgroundColor: 'transparent',
-						borderRadius: '5px',
-						border: '1px solid #FFFFFF',
-						color: '#FFFFFF',
-						fontWeight: 600,
-						fontSize: '18px',
-					}}
-				>
-					{content}
-				</Text>
-				<Text
-					style={{
-						fontSize: '14px',
-						margin: '24px 0',
-						color: '#ABABAB',
-						marginTop: '4px',
-						marginBottom: '12px',
-					}}
-				>
-					Maintenant, tu peux faire ce que tu veux avec ce message ! 🎉
-				</Text>
-				<Img
-					src="https://cuzeac-florin.app/og.png"
-					width="36"
-					height="36"
-					alt="Cuzeac Florin - Mon portfolio personnel"
-					style={{ borderRadius: '10px', marginTop: '18px' }}
-				/>
-				<Text
-					style={{
-						color: '#898989',
-						fontSize: '12px',
-						lineHeight: '22px',
-						marginTop: '12px',
-						marginBottom: '24px',
-					}}
-				>
-					Cuzeac Florin - Mon portfolio personnel - {new Date().getFullYear()}
-				</Text>
-			</Container>
-		</Body>
-	</Html>
+						{content}
+					</Text>
+					<Text className="mt-0.5 block text-gray-700 text-xs">
+						Maintenant, tu peux faire ce que tu veux avec ce message ! 🎉
+					</Text>
+
+					<Img
+						src="https://cuzeac-florin.app/og.png"
+						width="36"
+						height="36"
+						alt="Cuzeac Florin - Mon portfolio personnel"
+						style={{ borderRadius: '10px' }}
+						className="mt-10 object-cover object-center"
+					/>
+					<Text className="mt-1 block text-gray-700 text-sm">
+						<span className="font-bold text-yellow-300">Cuzeac Florin</span> -
+						Mon portfolio personnel -{' '}
+						<span className="font-bold text-yellow-300">
+							{new Date().getFullYear()}
+						</span>
+					</Text>
+				</Container>
+			</Body>
+		</Html>
+	</Tailwind>
 );
 
 export default ContactEmailTemplate;
