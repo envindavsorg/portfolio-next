e8ad3d4b12be2e3d439cff50378facbe23fd6c1f
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-16 09:34:28 +0200

    optimize components

--- app/auth.ts
+++ app/auth.ts
@@ -27,7 +27,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 	pages: {
 		signIn: '/login',
 	},
-	debug: process.env.NODE_ENV === 'development',
+	debug: false,
 	logger: {
 		error(code, ...message) {
 			logger.error(code, message);
