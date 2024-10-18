1bc778fe54a5c67e71237c7b7daadb2b4ec22e1b
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-18 15:26:16 +0200

    update & optimize => Next.js RC 2

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -3,7 +3,6 @@ import { Footer } from '@/components/navigation/Footer';
 import { NavBar } from '@/components/navigation/NavBar';
 import { NavBarProvider } from '@/components/navigation/NavBarProvider';
 import { SideNavBar } from '@/components/navigation/SideNavBar';
-import { navigation } from '@/resources/navigation';
 import type React from 'react';
 import { Suspense } from 'react';
 
@@ -12,11 +11,11 @@ interface RootLayoutProps {
 }
 
 const WebsiteLayout = async ({ children }: Readonly<RootLayoutProps>) => (
-	<div className="flex min-h-screen flex-col justify-between p-8 pt-0 text-foreground md:pt-8">
+	<div className="flex min-h-screen flex-col justify-between p-8 text-foreground pt-8">
 		<SideNavBar />
 		<main className="mx-auto w-full max-w-[60ch] pb-5">
 			<NavBarProvider>
-				<NavBar navigation={navigation} />
+				<NavBar />
 			</NavBarProvider>
 
 			{children}
