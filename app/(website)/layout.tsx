1bf5c566f6b8ccaedba4c09922f8196b58e19e8a
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-19 10:36:46 +0200

    update clean components

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,6 +1,6 @@
 import { Sparkles } from '@/components/background/Sparkles';
 import { Footer } from '@/components/navigation/Footer';
-import { NavBar, NavBarSlim } from '@/components/navigation/NavBar';
+import { NavBar } from '@/components/navigation/NavBar';
 import type React from 'react';
 
 interface RootLayoutProps {
@@ -10,11 +10,8 @@ interface RootLayoutProps {
 const WebsiteLayout = ({ children }: Readonly<RootLayoutProps>) => (
 	<div className="flex min-h-screen flex-col justify-between p-6 text-foreground sm:p-8">
 		<NavBar />
-		<main className="mx-auto w-full max-w-[60ch]">
-			<NavBarSlim />
-			{children}
-			<Footer />
-		</main>
+		<main className="mx-auto w-full max-w-[60ch]">{children}</main>
+		<Footer />
 
 		<Sparkles density={50} />
 	</div>
