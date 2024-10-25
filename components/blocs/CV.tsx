2944e9bb2162a54908d7278b222792ded6bca828
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-25 19:50:30 +0200

    update work page

--- components/blocs/CV.tsx
+++ components/blocs/CV.tsx
@@ -10,26 +10,28 @@ interface CVProps {
 
 export const CV = ({ className }: CVProps) => {
 	return (
-		<Link
-			href="https://drive.google.com/file/d/1mD3zdZeeg9q4sQooyd8R1bsfZ8Uw_NIt/view?usp=share_link"
-			target="_blank"
-			rel="noopener noreferrer"
-			className={cn(className, 'no-underline')}
-		>
-			<Button
-				className="flex w-max items-center gap-x-3"
-				size="lg"
-				variant="outline"
+		<div className={className}>
+			<Link
+				href="https://drive.google.com/file/d/1mD3zdZeeg9q4sQooyd8R1bsfZ8Uw_NIt/view?usp=share_link"
+				target="_blank"
+				rel="noopener noreferrer"
+				className={'no-underline'}
 			>
-				<span className="font-bold">
-					<span className="inline-block sm:inline-block min-[530px]:hidden">
-						Télécharger
+				<Button
+					className="flex w-max items-center gap-x-3"
+					size="lg"
+					variant="outline"
+				>
+					<span className="font-bold">
+						<span className="inline-block sm:inline-block min-[530px]:hidden">
+							Télécharger
+						</span>
+						<span className="max-[530px]:hidden sm:hidden">Voir</span>{' '}
+						<span className="font-extrabold text-theme">mon CV</span>
 					</span>
-					<span className="max-[530px]:hidden sm:hidden">Voir</span>{' '}
-					<span className="font-extrabold text-theme">mon CV</span>
-				</span>
-				<Download className="size-4 shrink-0" weight="duotone" />
-			</Button>
-		</Link>
+					<Download className="size-4 shrink-0" weight="duotone" />
+				</Button>
+			</Link>
+		</div>
 	);
 };
