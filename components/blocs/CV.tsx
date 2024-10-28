7677329adf761bdcdf57e8cb2e732a57d7f9e75f
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-28 18:00:16 +0100

    update homepage

--- components/blocs/CV.tsx
+++ components/blocs/CV.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/Button';
-import { cn } from '@/lib/utils';
 import { Download } from '@phosphor-icons/react/dist/ssr';
 import Link from 'next/link';
 import type React from 'react';
@@ -9,29 +8,5 @@ interface CVProps {
 }
 
 export const CV = ({ className }: CVProps) => {
-	return (
-		<div className={className}>
-			<Link
-				href="https://drive.google.com/file/d/1mD3zdZeeg9q4sQooyd8R1bsfZ8Uw_NIt/view?usp=share_link"
-				target="_blank"
-				rel="noopener noreferrer"
-				className={'no-underline'}
-			>
-				<Button
-					className="flex w-max items-center gap-x-3"
-					size="lg"
-					variant="outline"
-				>
-					<span className="font-bold">
-						<span className="inline-block sm:inline-block min-[530px]:hidden">
-							Télécharger
-						</span>
-						<span className="max-[530px]:hidden sm:hidden">Voir</span>{' '}
-						<span className="font-extrabold text-theme">mon CV</span>
-					</span>
-					<Download className="size-4 shrink-0" weight="duotone" />
-				</Button>
-			</Link>
-		</div>
-	);
+	return <div className={className}></div>;
 };
