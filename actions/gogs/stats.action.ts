93e15a0cc6f8c6914a7fff0515743913ba05113a
Cuzeac Florin <cuzeacflo@gmail.com>
2024-10-29 14:23:02 +0100

    update homepage

--- actions/gogs/stats.action.ts
+++ actions/gogs/stats.action.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { logger } from '@/lib/logger';
+import { unstable_noStore as noStore } from 'next/cache';
 
 const headers = {
 	'Content-Type': 'application/json',
@@ -8,6 +9,8 @@ const headers = {
 };
 
 export const gogsStats = async (url: string): Promise<Response[]> => {
+	noStore();
+
 	if (!url) {
 		logger.error('→ url parameter is required !');
 		throw new Error('→ GOGS_URL env variable is not set ...');
