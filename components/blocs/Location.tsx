7cf55c5cbb6dc81dc79a8c27548776e83ee7e6eb
Cuzeac Florin <cuzeacflo@gmail.com>
2024-11-04 11:03:27 +0100

    update homepage

---
+++ components/blocs/Location.tsx
@@ -0,0 +1,32 @@
+// Copyright Cuzeac Florin 2024. All Rights Reserved.
+// Project: portfolio-site
+// Author contact: https://www.linkedin.com/in/cuzeacflorin/
+// This file is licensed under the MIT Licence.
+// Licence text available at https://opensource.org/licenses/MIT
+
+'use client';
+
+import { FadeIn } from '@/components/animations/FadeIn';
+import { display } from '@/resources/config';
+import dynamic from 'next/dynamic';
+import type React from 'react';
+
+const LocationWidget = dynamic(
+	() =>
+		import('@/components/widgets').then((mod) => ({
+			default: mod.LocationWidget,
+		})),
+	{
+		ssr: false,
+	},
+);
+
+export const Location = (): React.JSX.Element => (
+	<>
+		{display.location && (
+			<FadeIn className="mt-3 flex w-full flex-col sm:mt-0">
+				<LocationWidget />
+			</FadeIn>
+		)}
+	</>
+);
