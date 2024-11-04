8ca2435ea99e687f499a404ec3fb2827079b82aa
Cuzeac Florin <cuzeacflo@gmail.com>
2024-11-04 11:03:05 +0100

    update homepage

--- elements/home/index.ts
+++ elements/home/index.ts
@@ -1,5 +1,3 @@
 export { BlogArticles } from './BlogArticles';
-export { LandingBlock } from './LandingBlock';
 export { ProjectStars } from './ProjectStars';
 export { SocialSubs } from './SocialSubs';
-export { TechStack } from './TechStack';
